2a757682bddb42913da77cf44898723427b443cd
damnisohigh <pashagoncharovjp2@gmail.com>
2025-08-31 16:51:53 +0300

    Initial Commit

---
+++ RouletteGame/RouletteGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  RouletteGameApp.swift
+//  RouletteGame
+//
+//  Created by DAMNISOHIGH on 31.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct RouletteGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
